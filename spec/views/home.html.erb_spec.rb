42cf15b4126c1f5eaea03c75d59e98c809795007
Gilbert Torchon <gilbert@noukod.com>
2019-01-30 22:09:37 +0000

    Automatically backed up by Learn

--- spec/views/home.html.erb_spec.rb
+++ spec/views/home.html.erb_spec.rb
@@ -2,7 +2,7 @@ require "spec_helper"
 
 describe "welcome/home.html.erb" do
   it "displays all the widgets" do
-    render "/"
+    visit "/"
     expect(rendered).to have_link('Login with Facebook', :href => '/auth/facebook')
   end
 end
